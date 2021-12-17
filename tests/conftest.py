416b4911f949b62c17431ce5b51f7ff05db9855a
xinyao qian <zceexqi@ucl.ac.uk>
2021-12-17 15:42:25 +0000

    9 tests finished

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,41 +3,49 @@
 # @Time    :    17/12/2021 01:07
 # @Author  :    Xinyao Qian
 # @SN      :    19021373
-# @Description: 
+# @Description: This file is pytest fixture to set up the data for test_user.py
+                There are 5 fixtures in total
 """
 import pytest
 from user import User
 import datetime
+
+
 @pytest.fixture(scope='function')
 def normal_user_without_birthday():
-    user_no_bod = User(first_name='Xinyao',last_name='Qian',
-                      email='qianxinyao@gmail.com',password='123345')
+    user_no_bod = User(first_name='Xinyao', last_name='Qian',
+                       email='qianxinyao@gmail.com', password='123345')
     yield user_no_bod
+
 
 @pytest.fixture(scope='function')
 def normal_user_with_birthday():
-    user_with_bod = User(first_name='Moon',last_name='Sun',
-                      email='MoonSun@gmail.com',password='1414314',
+    user_with_bod = User(first_name='Moon', last_name='Sun',
+                         email='MoonSun@gmail.com', password='1414314',
                          dob=datetime.datetime(2000, 6, 1))
     yield user_with_bod
 
+
 @pytest.fixture(scope='function')
 def user_with_wrong_email():
-    user_with_wrong_email = User(first_name='Fish',last_name='Gomes',
-                      email='Hello_World',password='123141343',
-                         dob=datetime.datetime(2000, 10, 11))
+    user_with_wrong_email = User(first_name='Fish', last_name='Gomes',
+                                 email='Hello_World', password='123141343',
+                                 dob=datetime.datetime(2000, 10, 11))
     yield user_with_wrong_email
+
 
 @pytest.fixture(scope='function')
 def normal_user_with_abnormal_birthday():
-    user_with_abnormal_dob = User(first_name='Sally',last_name='Day',
-                      email='SallyDay@gmail.com',password='1345135',
-                         dob=datetime.datetime(1000, 6, 1))
+    user_with_abnormal_dob = User(first_name='Sally', last_name='Day',
+                                  email='SallyDay@gmail.com',
+                                  password='1345135',
+                                  dob=datetime.datetime(1000, 6, 1))
     yield user_with_abnormal_dob
+
 
 @pytest.fixture(scope='function')
 def user_name_with_number():
-    user_name_number =User(first_name='2713819',last_name='123123',
-                      email='111@gmail.com',password='13432sad5',
-                         dob=datetime.datetime(2005, 6, 11))
+    user_name_number = User(first_name='2713819', last_name='123123',
+                            email='111@gmail.com', password='13432sad5',
+                            dob=datetime.datetime(2005, 6, 11))
     yield user_name_number
--- tests/test_user.py
+++ tests/test_user.py
@@ -4,6 +4,7 @@
 # @Author  :    Xinyao Qian
 # @SN      :    19021373
 # @Description: This file is associated with test called user.py
+                There are 9 tests in total.
 """
 from datetime import datetime
 
@@ -11,91 +12,105 @@ import pytest
 
 
 def test_full_name_is_created_when_user_created(normal_user_without_birthday):
-    '''
-    GIVEN a user
-    WHEN first_name is Xinyao last_name is Qian
-    THEN the create_full_name should return Xinyao Qian
-    '''
+    """
+    GIVEN a user is created
+    And first_name is Xinyao,  last_name is Qian
+    WHEN first_name and last_name are passed to create_full_name function
+    THEN the result should return Xinyao Qian
+    """
     full_name = normal_user_without_birthday.create_full_name()
     assert full_name == 'Xinyao Qian'
 
 
-def test_age_not_calculated_when_user_has_no_bod(normal_user_without_birthday):
-    '''
-    GIVEN a user
-    WHEN first_name is Xinyao last_name is Qian
-    THEN the create_full_name should return Xinyao Qian
-    '''
+def test_age_not_calculated_when_user_has_no_dob(normal_user_without_birthday):
+    """
+    GIVEN a user is created
+    And Date of birth is null
+    WHEN calculate_age function is called
+    THEN a warning message should appear
+    """
     age = normal_user_without_birthday.calculate_age()
     assert age == 'Age not calculated, date of birth unknown'
 
 
-def test_age_is_calculated_when_user_has_bod(normal_user_with_birthday):
-    '''
-    GIVEN a user
-    WHEN first_name is Xinyao last_name is Qian
-    THEN the create_full_name should return Xinyao Qian
-    '''
+def test_age_is_calculated_when_user_has_dob(normal_user_with_birthday):
+    """
+    GIVEN a user is created
+    And Date of birth is 2000, 6, 1
+    When calculate_age function is called
+    THEN age should return 21
+    """
+
     age = normal_user_with_birthday.calculate_age()
     assert age == 21
 
 
-def test_password_match_is_True_when_correct_password_entered(
+def test_age_is_warned_when_user_has_abnormal_dob(
+        normal_user_with_abnormal_birthday):
+    """
+    GIVEN a user is created
+    And Date of birth is 1000, 6, 1
+    When calculate_age function is called
+    THEN an exception should raise
+    """
+    with pytest.raises(Exception) as exp:
+        normal_user_with_abnormal_birthday.calculate_age()
+
+
+def test_age_is_invalid_when_user_has_dob_in_future(
+        normal_user_with_abnormal_birthday):
+    """
+    GIVEN a user is created
+    And Date of birth is 2025, 6, 1
+    When calculate_age function is called
+    THEN an exception should raise
+    """
+    with pytest.raises(Exception) as exp:
+        normal_user_with_abnormal_birthday.dob = datetime.datetime(2025, 6, 1)
+        normal_user_with_abnormal_birthday.calculate_age()
+
+
+def test_password_match_is_true_when_correct_password(
         normal_user_with_birthday):
-    '''
-    GIVEN a user
-    WHEN first_name is Xinyao last_name is Qian
-    THEN the create_full_name should return Xinyao Qian
-    '''
+    """
+      GIVEN a user is created
+      And password is '1414314'
+      When is_correct_password function is called
+      And entered password is '1414314'
+      THEN result should return True
+      """
     match = normal_user_with_birthday.is_correct_password('1414314')
     assert match is True
 
 
 def test_password_match_is_False_when_wrong_password_entered(
         normal_user_with_birthday):
-    '''
-    GIVEN a user
-    WHEN first_name is Xinyao last_name is Qian
-    THEN the create_full_name should return Xinyao Qian
-    '''
+    """
+      GIVEN a user is created
+      And password is '1414314'
+      When is_correct_password function is called
+      And entered password is '1234325'
+      THEN result should return False
+      """
     match = normal_user_with_birthday.is_correct_password('1234325')
     assert match is False
 
 
 def test_email_is_not_right_when_user_is_created(normal_user_with_birthday):
-    '''
-     GIVEN a user
-     WHEN first_name is Xinyao last_name is Qian
-     THEN the create_full_name should return Xinyao Qian
-     '''
+    """
+      GIVEN user email is '73891273'
+      When a new user is created
+      THEN an exception should raise indicating a wrong format
+      """
     with pytest.raises(Exception) as exp:
         normal_user_with_birthday.email = '73891273'
 
 
-def test_age_is_warned_when_user_has_bod(normal_user_with_abnormal_birthday):
-    '''
-    GIVEN a user
-    WHEN first_name is Xinyao last_name is Qian
-    THEN the create_full_name should return Xinyao Qian
-    '''
-    with pytest.raises(Exception) as exp:
-        age = normal_user_with_abnormal_birthday.calculate_age()
-
 def test_name_is_invalid_when_containing_number(user_name_with_number):
     '''
-    GIVEN a user
-    WHEN first_name is Xinyao last_name is Qian
-    THEN the create_full_name should return Xinyao Qian
+    GIVEN a user first_name is '123' and last_name is '123123'
+    WHEN the user is created
+     THEN an exception should raise indicating a wrong format of name
     '''
     with pytest.raises(Exception) as exp:
-        user_name_with_number.first_name = 123
-
-def test_age_is_warned_when_user_has_bod(normal_user_with_abnormal_birthday):
-    '''
-    GIVEN a user
-    WHEN first_name is Xinyao last_name is Qian
-    THEN the create_full_name should return Xinyao Qian
-    '''
-    with pytest.raises(Exception) as exp:
-        normal_user_with_abnormal_birthday.dob = datetime.datetime(2025, 6, 1)
-        age = normal_user_with_abnormal_birthday.calculate_age()
+        user_name_with_number.first_name = '123'
