e9208943324e6dfae6d345eb104f064253f86cf0
xinyao qian <zceexqi@ucl.ac.uk>
2021-12-18 13:06:26 +0000

    adding description for requirements part

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,20 +7,20 @@
                 There are 5 fixtures in total
 """
 import pytest
-import user
+from user import User
 import datetime
 
 
 @pytest.fixture(scope='function')
 def normal_user_without_birthday():
-    user_no_bod = user.User(first_name='Xinyao', last_name='Qian',
+    user_no_bod = User(first_name='Xinyao', last_name='Qian',
                        email='qianxinyao@gmail.com', password='123345')
     yield user_no_bod
 
 
 @pytest.fixture(scope='function')
 def normal_user_with_birthday():
-    user_with_bod = user.User(first_name='Moon', last_name='Sun',
+    user_with_bod = User(first_name='Moon', last_name='Sun',
                          email='MoonSun@gmail.com', password='1414314',
                          dob=datetime.datetime(2000, 6, 1))
     yield user_with_bod
@@ -28,7 +28,7 @@ def normal_user_with_birthday():
 
 @pytest.fixture(scope='function')
 def user_with_wrong_email():
-    user_with_wrong_email = user.User(first_name='Fish', last_name='Gomes',
+    user_with_wrong_email = User(first_name='Fish', last_name='Gomes',
                                  email='Hello_World', password='123141343',
                                  dob=datetime.datetime(2000, 10, 11))
     yield user_with_wrong_email
@@ -36,7 +36,7 @@ def user_with_wrong_email():
 
 @pytest.fixture(scope='function')
 def normal_user_with_abnormal_birthday():
-    user_with_abnormal_dob = user.User(first_name='Sally', last_name='Day',
+    user_with_abnormal_dob = User(first_name='Sally', last_name='Day',
                                   email='SallyDay@gmail.com',
                                   password='1345135',
                                   dob=datetime.datetime(1000, 6, 1))
@@ -45,7 +45,7 @@ def normal_user_with_abnormal_birthday():
 
 @pytest.fixture(scope='function')
 def user_name_with_number():
-    user_name_number = user.User(first_name='2713819', last_name='123123',
+    user_name_number = User(first_name='2713819', last_name='123123',
                             email='111@gmail.com', password='13432sad5',
                             dob=datetime.datetime(2005, 6, 11))
     yield user_name_number
